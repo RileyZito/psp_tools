e41a94f8e7ecffce17c3eee5315a9bc6403679f9
Mellie Zito <melliezito@comcast.net>
2017-07-21 10:44:43 -0400

    Added copyright/license and my name and date of creation

--- finder.py
+++ finder.py
@@ -1,4 +1,12 @@
-#This is the automated code
+#!/usr/bin/python2
+# Copyright (C) 2015 OCEAN collaboration
+#
+# This file is part of the OCEAN project and distributed under the terms 
+# of the University of Illinois/NCSA Open Source License. See the file 
+# `License' in the root directory of the present distribution.
+#
+#Melanie Zito. July 2017
+
 import shutil, os, sys, bisect
 
 commona = open("Common/pp.dir", "r")
