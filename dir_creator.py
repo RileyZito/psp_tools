1be2a4898371ef959ab7253e412d1507917bbce8
Mellie Zito <melliezito@comcast.net>
2017-07-26 15:38:40 -0400

    Code now writes all necessary files for each md5 directory.

--- dir_creator.py
+++ dir_creator.py
@@ -39,6 +39,53 @@ print md5_fhi_list
 print "\n"
 #grabs all md5's from table. Currently a bit broken because it will add None
 
+
+
+
+
+def file_creator(type):
+	#takes string input of file name like "opts"
+	
+	type_name = type + "_name"
+	cursor.execute( ''' SELECT ''' + type_name + ''' FROM pseudos WHERE md5_fhi=? ''', (md5_fhi,))
+	#have to connect strings because ? doesn't work for column or table names
+
+	retrieved = cursor.fetchone()[0]
+	if retrieved is None:
+		print "There is no " + type  + "\n"
+		info_location = os.path.join(md5_directory, "info.txt")
+                with open(info_location, "a") as info:
+                	info.write("\n")
+		#for that file, just a line return is entered in info.txt
+
+	else:
+        	name = retrieved.encode('ascii', 'ignore')
+        	print type + " file name is: " + name
+
+        	cursor.execute( ''' SELECT ''' + type + ''' FROM pseudos WHERE md5_fhi=? ''', (md5_fhi,))
+
+        	retrieved = cursor.fetchall()[0]
+        	data = str(retrieved[0])
+        	#gets file name and text from database and saves it as variables
+
+        	location = os.path.join(md5_directory, name)
+        	with open(location, "w") as one_file:
+        		one_file.write(data)
+        	print name + " was written.\n"
+        	#writes found information to a created file with found name
+	
+
+		info_location = os.path.join(md5_directory, "info.txt")
+		with open(info_location, "a") as info:
+			info.write(name + "\n")
+		print "info.txt was written.\n"
+		#for each file created, that file name is added to info.txt
+#to write files if an entry exists in the database
+
+
+
+
+
 for md5_fhi in md5_fhi_list:
 	md5_directory = os.path.join(user_location, md5_fhi)
 	os.makedirs(md5_directory)
@@ -46,7 +93,7 @@ for md5_fhi in md5_fhi_list:
 
 	cursor.execute( ''' SELECT id FROM pseudos WHERE md5_fhi=? ''', (md5_fhi,))
 	id = cursor.fetchone()[0]
-	print "id: " + str(id)
+	print "\nid: " + str(id)
 	#grabs id using md5 so that id can be used for main table
 
 	#znucl:
@@ -55,7 +102,7 @@ for md5_fhi in md5_fhi_list:
 	z_location = os.path.join(md5_directory, "znucl")
         with open(z_location, "w") as znucl:
         	znucl.write(str(z))
-        print "znucl file for " + str(z) + " was written.\n"
+        print str(z) + " was written to znucl file."
 
 	#quality:
 	cursor.execute( ''' SELECT qf FROM main WHERE id=? ''', (id,))
@@ -63,9 +110,62 @@ for md5_fhi in md5_fhi_list:
         quality_location = os.path.join(md5_directory, "quality")
         with open(quality_location, "w") as quality:
         	quality.write(str(qf))
-        print "quality file for " + str(qf) + " was written.\n"
+        print str(qf) + " was written to quality file."
 
 	#semicore:
+        cursor.execute( ''' SELECT semicore FROM main WHERE id=? ''', (id,))
+        retrieved = cursor.fetchone()[0]
+	semicore = retrieved.encode('ascii', 'ignore')
+        semicore_location = os.path.join(md5_directory, "semicore")
+        with open(semicore_location, "w") as sc:
+                sc.write(semicore)
+        print semicore + " was written to semicore file.\n"
+
+	#fhi file:
+	cursor.execute( ''' SELECT fhi_name FROM pseudos WHERE md5_fhi=? ''', (md5_fhi,))
+        
+	retrieved = cursor.fetchone()[0]
+        fhi_name = retrieved.encode('ascii', 'ignore')
+	print "fhi file name is: " + fhi_name
+
+	cursor.execute( ''' SELECT fhi FROM pseudos WHERE md5_fhi=? ''', (md5_fhi,))
+
+	retrieved = cursor.fetchall()[0]
+	fhi_text = str(retrieved[0])
+	#gets fhi file name and text from database and saves it as variables
+
+	fhi_location = os.path.join(md5_directory, fhi_name)
+        with open(fhi_location, "w") as fhi:
+                fhi.write(fhi_text)
+        print fhi_name + " was written.\n"
+	#writes found fhi information to a created fhi file with found fhi name
+
+	
+	#info.txt:
+        info_location = os.path.join(md5_directory, "info.txt")
+        with open(info_location, "w") as info:
+        	info.write(fhi_name + "\n")
+	#other files are written in file_creator
+
+
+	#for other files:
+
+	file_creator("upf")
+	file_creator("fill")
+	file_creator("opts")
+
+
+	#citation:
+	cursor.execute( ''' SELECT citation FROM pseudos WHERE md5_fhi=? ''', (md5_fhi,))
+	
+	retrieved = cursor.fetchall()[0]
+        citation_text = str(retrieved[0])
+
+	citation_location = os.path.join(md5_directory, "citation")
+        with open(citation_location, "w") as citation:
+                citation.write(citation_text)
+        print "Citation was written.\n"
+#writes all of the files in each md5_fhi directory
 
 
 
