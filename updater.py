9a03f75122dbbf54766c8fb16a9f0a23534b0dea
Mellie Zito <melliezito@comcast.net>
2017-07-27 16:53:04 -0400

    Now checks over already existing md5 fhi's and compares database files to directory files. Updates database with new information. Spacing is better and fixed issue with calculating md5 upf.

--- updater.py
+++ updater.py
@@ -27,7 +27,7 @@ print user_location
 
 def file_updater(type):
 	#takes string input like "opts"
-	print "Adding " + type + " file to database."
+	print "\nAdding " + type + " file to database."
 	#just to see what's what
 
 	if upf_name[-3:] == type.upper():
@@ -63,16 +63,110 @@ def file_updater(type):
 			#if type_file is the upf, its md5 upf needs to be added too.
 	
 			if type_file == upf_name:
+				with open(type_location, 'r') as file:
+					data = file.read()
+
 				m_upf = hashlib.md5(data)
 				hash_upf = m_upf.hexdigest()
 				print "UPF md5: " + hash_upf
-				#calculates md5
+			#calculates upf md5
 
 				cursor.execute(''' UPDATE pseudos SET md5_upf=? WHERE md5_fhi=? ''', (hash_upf, hash,))
 
 		else:
 			print "NOTICE: info.txt said file existed but " + type_file + "does not exist in directory."
 			print type_file + " will not be added to the database."
+#to add new files to database
+
+
+
+
+
+
+def file_name_checker(type):
+	type_name = type + "_name"
+        print "\n" + type_name 	
+
+	if upf_name[-3:] == type.upper():
+                file_name = upf_name
+        elif opts_name[-4:] == type:
+                file_name = opts_name
+        elif fill_name[-4:] == type:
+                file_name = fill_name
+        else:
+                print "Something went wrong with file_name_checker."
+                sys.exit(1)
+        #to figure out which type got entered and then get that file name
+	
+	print "File name: " + file_name
+
+	if file_name[0:6] == "blank.":
+                print type + " does not exist. The database won't be checked."
+
+        else:
+
+		cursor.execute( ''' SELECT ''' + type_name + ''' FROM pseudos WHERE md5_fhi=?''', (hash,))
+		retrieved = cursor.fetchone()[0]
+		database_type_name = retrieved.encode('ascii', 'ignore') 
+		#gets name listed for file in database
+
+
+		if file_name != database_type_name:
+			cursor.execute( ''' UPDATE pseudos SET ''' + type_name + '''=? WHERE md5_fhi=? ''', (file_name, hash,))
+			print "Database was updated with new " + type + " name."
+		#if name found in info.txt doesn't match name in database, write new name to database
+
+#checks that file name in database is up to date
+
+
+
+
+
+def file_checker(type):
+	print "\n" + type
+	if upf_name[-3:] == type.upper():
+                type_file_name = upf_name
+        elif opts_name[-4:] == type:
+                type_file_name = opts_name
+        elif fill_name[-4:] == type:
+                type_file_name = fill_name
+        elif "citation" == type:
+                type_file_name = citation_name
+        else:
+                print "Something went wrong with file_checker."
+                sys.exit(1)
+	
+	print "File name: " + type_file_name
+
+        if type_file_name[0:6] == "blank.":
+                print type + " does not exist. The database won't be checked."
+
+        else:
+		cursor.execute( ''' SELECT ''' + type + ''' FROM pseudos WHERE md5_fhi=?''', (hash,))
+		retrieved = cursor.fetchall()[0]	
+		database_text = retrieved[0]
+		#gets text for type from database
+ 	
+
+		type_file = os.path.join(user_location, dir_md5, type_file_name)
+        	with open(type_file, "r") as file:
+        		file_text = file.read() 		
+
+		if str(file_text) != database_text:
+			cursor.execute( ''' UPDATE pseudos SET ''' + type + '''=? WHERE md5_fhi=? ''', (file_text, hash,))
+			print "Database was updated with new " + type + " information."
+		#compares text from database to text in file from directory, updates database with changes
+		
+			if type_file_name == upf_name:
+				m_upf = hashlib.md5(file_text)
+                        	hash_upf = m_upf.hexdigest()
+                        	print "md5 UPF: " + hash
+ 			cursor.execute( ''' UPDATE pseudos SET md5_upf=? WHERE md5_fhi=? ''', (hash_upf, hash,))
+                        #calculates new md5 for UPF file
+
+
+#checks a file to see if it's up to date compared to the database
+
 
 
 
@@ -96,8 +190,9 @@ print "\n"
 dir_md5_fhi_list = os.listdir(user_location)
 
 for dir_md5 in dir_md5_fhi_list:
+
 	if dir_md5 not in md5_fhi_list:
-		print "The database does not have " + dir_md5 + " yet."
+		print "\n\nNOT IN DATABASE: " + dir_md5 + "\nDatabase will be populated with new entry."
 		info_file = os.path.join(user_location, dir_md5, "info.txt")
 		with open(info_file, "r") as info:
 			searchlines = info.readlines()
@@ -122,7 +217,7 @@ for dir_md5 in dir_md5_fhi_list:
 		#goes through info.txt file and grabs names of different files. if file isn't there it sets file = ""
 
 		#populating database:
-		print "fhi file: " + fhi_name + "\n"
+		print "\nfhi file: " + fhi_name
 		fhi_file = os.path.join(user_location, dir_md5, fhi_name)
 
 		if os.path.isfile(fhi_file) == True:
@@ -166,7 +261,7 @@ for dir_md5 in dir_md5_fhi_list:
 			file_updater("fill")
 			#files needed to be added to database too but only if they exist in directory
 			
-			print "Entries were added to database for " + dir_md5
+			print "\nEntries were added to database for " + dir_md5
 
 			#cite:
 			citation_file = os.path.join(user_location, dir_md5, "cite")
@@ -176,12 +271,126 @@ for dir_md5 in dir_md5_fhi_list:
 			#opens citation file and grabs everything from it
 			
 			cursor.execute( '''UPDATE pseudos SET citation=? WHERE md5_fhi=? ''', (cite, hash,))
-
+	#if md5 fhi wasn't in database
+	
 				
+
 	elif dir_md5 in md5_fhi_list:
-		print "Still need to do this.\n"
-	#check over everything in dir_md5 and make sure database info is up to date.
-	#md5 should be calculated at start to make sure that dir_md5 is correct/the .fhi is correct
+		print "\n\nALREADY IN DATABASE: " + dir_md5 + "\nDirectory will be checked for updates.\n"
+		
+		info_file = os.path.join(user_location, dir_md5, "info.txt")
+                with open(info_file, "r") as info:
+                        searchlines = info.readlines()
+                        for i, line in enumerate(searchlines):
+                                if ".fhi" in line:
+                                        for l in searchlines[0:i]: fhi_name = l.split()[0]
+                                        for l in searchlines[i:i+1]:
+                                                if l == "\n":
+                                                        upf_name = "blank.upf"
+                                                else:
+                                                        upf_name = l.split()[0]
+					for l in searchlines[i+1:i+2]:
+                                                if l == "\n":
+                                                        opts_name = "blank.opts"
+                                                else:
+                                                        opts_name = l.split()[0]
+                                        for l in searchlines[i+2:i+3]:
+                                                if l == "\n":
+                                                        fill_name = "blank.fill"
+                                                else:
+                                                        fill_name = l.split()[0]
+					for l in searchlines[i+3:i+4]:
+                                                if l == "\n":
+                                                        citation_name = "blank.fill"
+                                                else:
+                                                        citation_name = l.split()[0]
+                #goes through info.txt file and grabs names of different files. if file isn't there it sets file = ""
+
+					
+		print "fhi file: " + fhi_name
+                fhi_file = os.path.join(user_location, dir_md5, fhi_name)
+
+                if os.path.isfile(fhi_file) == True:
+                        with open(fhi_file, 'r') as file:
+                            data = file.read()
+                        #opens fhi file and grabs everything from it
+
+                        m = hashlib.md5(data)
+                        hash = m.hexdigest()
+                        print "md5: " + hash
+                        #calculates md5
+
+
+			if dir_md5 != hash:
+                		print dir_md5 + " is incorrectly named. The md5 calculation returned a different answer."
+                		sys.exit(1)
+                	#if the directory's md5 didn't match the one calculated from it's fhi file, code ends     
+		
+		file_name_checker("upf")
+		file_checker("upf")
+	
+		file_name_checker("opts")
+		file_checker("opts")
+
+		file_name_checker("fill")
+		file_checker("fill")
+
+		file_checker("citation")
+		#checks that info and names in database are up to date and updates database if needed		
+
+		cursor.execute( ''' SELECT id FROM pseudos WHERE md5_fhi=?''', (hash,))
+
+                id = cursor.fetchone()[0]
+		#gets id from pseudos so it knows what id to check in main
+
+		file_location = os.path.join(user_location, dir_md5, "znucl")
+                with open(file_location, "r") as znucl:
+                        file_z = znucl.read() 
+		
+		file_location = os.path.join(user_location, dir_md5, "quality")
+                with open(file_location, "r") as quality:
+                        file_qf = quality.read()
+
+		file_location = os.path.join(user_location, dir_md5, "semicore")
+                with open(file_location, "r") as sc:
+                        file_semicore = sc.read()
+
+		cursor.execute( ''' SELECT z FROM main WHERE id=?''', (id,))
+		retrieved = cursor.fetchone()		
+		database_z = retrieved[0]
+
+		cursor.execute( ''' SELECT qf FROM main WHERE id=?''', (id,))
+		retrieved = cursor.fetchone()
+		database_qf = retrieved[0]
+
+                cursor.execute( ''' SELECT semicore FROM main WHERE id=?''', (id,))
+		retrieved = cursor.fetchone()
+		database_semicore = retrieved[0].encode('ascii', 'ignore')
+
+		print "\nz from file:" + file_z
+		print "z from database:" + str(database_z) 
+		if file_z != str(database_z):
+			cursor.execute( ''' UPDATE main SET z=? WHERE id=? ''', (file_z, id,))			
+			print "z was updated in database."
+	
+		print "\nqf from file:" + file_qf
+		print "qf from database:" + str(database_qf)
+		if file_qf != str(database_qf):
+			cursor.execute( ''' UPDATE main SET qf=? WHERE id=? ''', (file_qf, id,))
+			print "qf was updated in database."
+
+		print "\nsemicore from file:" + file_semicore
+                print "semicore from database:" + database_semicore
+                if file_semicore != database_semicore:
+                        cursor.execute( ''' UPDATE main SET semicore=? WHERE id=? ''', (file_semicore, id,))
+			print "semicore was updated in database."
+
+		#z, qf, and semicore- make sure info in those files matches up with info in main
+
+		
+	#if md5 fhi was in database, check over everything in dir_md5 and make sure database info is up to date.
+
+
 
 
 db.commit()
